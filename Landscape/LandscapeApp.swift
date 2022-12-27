a93a3a6ea493394c3679524d7ad5273741f10c6b
Aitolkun <totita@list.ru>
2022-12-27 20:05:20 +0300

    Initial Commit

---
+++ Landscape/LandscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandscapeApp.swift
+//  Landscape
+//
+//  Created by Айтолкун Анарбекова on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LandscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
